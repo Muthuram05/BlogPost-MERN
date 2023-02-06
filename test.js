f157e07d1ecc926579e148ac157924a5ef4ddece
muthu ram <mr032495@gmail.com>
2023-02-06 22:39:32 +0530

    testing

---
+++ test.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+const userContoller= require('../controllers/users');
+const dbo = require('../db')
+
+
+router.get(['/',"/login"],(req,res)=>{
+    res.render("login")
+})
+router.get('/register',(req,res)=>{
+    res.render("Register")
+})
+router.get('/profile',userContoller.isLoggedIn,async(req,res)=>{
+    if(req.user){
+        let database =await dbo.getDataBase();
+        const collection = database.collection('datas');
+        const cursor = collection.find({email:req.user.email})
+        let datas =await cursor.toArray();
+        res.render("profile",{user:req.user,datas})
+    }
+    else{
+        res.redirect("/login")
+    }
+})
+router.get('/home',userContoller.isLoggedIn,async(req,res)=>{
+    if(req.user){
+        let database =await dbo.getDataBase();
+        const collection = database.collection('datas');
+        const cursor = collection.find({})
+        let datas =await cursor.toArray();
+        res.render("Home",{user:req.user,datas});
+    }
+    else{
+        res.redirect("/login")
+    }
+})
+router.get('/write',userContoller.isLoggedIn,async(req,res)=>{
+    if(req.user){
+        res.render("NewPost",{user:req.user});
+    }
+    else{
+        res.redirect("/login")
+    }
+})
+router.get('/logout',(req,res)=>{
+        res.cookie("Ram","logout",{
+        expires:new Date(Date.now()+2*1000),
+        httpOnly:true,
+    });
+    res.status(200).redirect("/");
+})
+router.get('/userprofile/:email',userContoller.isLoggedIn,async(req,res)=>{
+    let database =await dbo.getDataBase();
+        const collection = database.collection('datas');
+        const cursor = collection.find({email:req.params.email})
+        let datas =await cursor.toArray();
+        console.log()
+        res.render("userProfile",{user:req.user,datas,user_name:datas[0].name,user_email:datas[0].email});
+})
+module.exports = router;
